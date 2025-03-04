8a593741eba727771aae54d01ce0142a2697455b
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-03-04 21:53:21 +0900

    first commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react';
+// import './App.css'
+
+// type Todo = {
+//   value: string;
+// };
+
+// export const App = () => {
+//   const [text, setText] = useState('');
+//   const [todos, setTodos] = useState<Todo[]>([]);
+
+//   return (
+//     <div>
+//       <form onSubmit={(e) => e.preventDefault()}>
+//         <input type="test" value={text} onChange={(e) => setText(e.target.value)} />
+//         <input type="submit" value="追加" onSubmit={(e) => e.preventDefault()} />
+//       </form>
+
+//       <p>{text}</p>
+//     </div>
+//   );
+// }
+
