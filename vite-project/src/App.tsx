098b1c0721fe4f40bea01f66cf4f13e471d2f6a5
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-03-05 22:13:13 +0900

    formタグとinputタグでのSubmitの扱い方の違い

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -9,14 +9,27 @@ export const App = () => {
   const [text, setText] = useState('');
   const [todos, setTodos] = useState<Todo[]>([]);
 
+  const handleSubmit = () => {
+    if (!text) return
+
+    // 新しいTodoを作成
+    const newTodo: Todo = {
+      value: text
+    }
+
+    setTodos((todos) => [newTodo, ...todos])
+    setText('')
+  };
+
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}>
         <input type="test" value={text} onChange={(e) => setText(e.target.value)} />
-        <input type="submit" value="追加" onSubmit={(e) => e.preventDefault()} />
+        <input type="submit" value="追加"/>
       </form>
-
-      <p>{text}</p>
     </div>
   );
 }
