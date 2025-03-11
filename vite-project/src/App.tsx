8150426865e18e8c3d4fb340a43267dd1ec6ad62
Kazuki.T <107445735+Kazuki0320@users.noreply.github.com>
2025-03-11 23:08:12 +0900

    一部修正

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import './App.css'
 
 type Todo = {
   value: string;
@@ -10,65 +9,59 @@ export const App = () => {
   const [text, setText] = useState('');
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  const handleSubmit = () => {
-    if (!text) return
-
-    // 新しいTodoを作成
-    const newTodo: Todo = {
-      value: text,
-      id: new Date().getTime()
-    }
-
-    setTodos((todos) => [newTodo, ...todos])
-    setText('')
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value)
-  }
+  const handleSubmit = () => {
+    if (!text) return;
+
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime(),
+    };
+
+    setTodos((todos) => [newTodo, ...todos]);
+    setText('');
+  };
 
   const handleEdit = (id: number, value: string) => {
     setTodos((todos) => {
       const newTodos = todos.map((todo) => {
         if (todo.id === id) {
-          todo.value = value
+          return { ...todo, value };
         }
-        return todo
-      })
-
-      // todos ステートが書き換えられていないかチェック
-      console.log('=== Original todos ===');
-      todos.map((todo) => {
-        console.log(`id: ${todo.id}, value: ${todo.value}`);
+        return todo;
       });
 
       return newTodos;
-    })
-  }
+    });
+  };
 
   return (
     <div>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        handleSubmit();
-      }}>
-        <ul>
-          {todos.map((todo) => {
-            return (
-              <li key={todo.id}>
-                <input
-                  type="text"
-                  value={todo.value}
-                  onChange={(e) => handleEdit(todo.id, e.target.value)}
-                />
-              </li>
-            )
-          })}
-        </ul>
-        <input type="test" value={text} onChange={(e) => handleChange(e)} />
-        <input type="submit" value="追加"/>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <input type="text" value={text} onChange={(e) => handleChange(e)} />
+        <input type="submit" value="追加" onSubmit={handleSubmit} />
       </form>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <input
+                type="text"
+                value={todo.value}
+                onChange={(e) => handleEdit(todo.id, e.target.value)}
+              />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
-}
-
+};
